781b5a1f2e149c1d5267611fb61d758fb99dfbc9
Bhavishya-73 <bhavishya7300@gmail.com>
2026-06-16 11:39:11 +0530

    Day 1: Learned print statements, variables, data types, operators, type casting, and user input
    This file includes examples of print, variable declaration, arithmetic/relational/logical operators, type casting, and handling user input.

---
+++ day1.py
@@ -0,0 +1,60 @@
+# First code 
+print("My self Bhavishya")
+
+#Variables
+
+name = "Bhavishya" 
+age = 19 
+
+print("my name is " , name ,"and i am " ,age, "years old")
+
+# data type 
+a = 22
+b = "hellow"
+c = True 
+d = 2.343
+print(type(a), type(b), type(c) , type(d))
+
+# Arithmetic operators
+
+num1 = 9
+num2 = 3
+sum = num1 + num2 
+diff = num1 - num2 
+mul = num1 * num2 
+div = num1 / num2 
+modula = num1%num2
+print("sum :", sum , "diff:" , diff, "mul : ", mul , "div :", div , "modula :", modula)
+
+# Relational operator
+
+print(num1>num2)
+print(num1<num2)
+print(num1 >= num2)
+
+# Assigment operators
+
+e = 6
+e += 4
+print(e)
+
+#logical operators ( and, or, not)
+
+g = 5
+h = 6
+k = 7
+print( g<h and g>k )
+print (g<h or g>k)
+print(not (g<h ))
+
+# type casting
+
+o ="2"
+l = 2
+z = int(o)
+print(l+z)
+
+#input in python
+namee = input(" Your name :")
+agee = int(input("your  age :"))
+print ("your name is ", namee , "and you are :", agee, "years old")
